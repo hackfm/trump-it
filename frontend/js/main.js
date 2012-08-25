e305a10a7cbbfcd1e38c36c96ba0c179cdd23bb8
David Whiting <davidaw@last.fm>
2012-08-25 01:19:25 +0100

    Restructure and socket shiz

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -12,21 +12,42 @@ $(function() {
         console.log('result', track, user);
     });
     
+    function startRound(topTracks) {
+        $("#login").hide();
+        $("#mainscreen").show();
+        for (var i=0;i<3;i++) {
+            var trk=topTracks[i];
+            $("#tracks").append(templates.trackElement(trk.artist,trk.track,trk.image, 0.004));
+        }
+        $("#tracks").find(".track").click(function() {
+            var title = $(this).find(".title").text();
+            var artist = $(this).find(".artist").text();
+            var value = $(this).data("score");
+            var image = $(this).find("img").attr("src");
+            
+            socket.emit("pick", 
+                {
+                  "artist":artist,
+                  "title":title,
+                  "image":image
+                },
+                value);
+            console.log("track_click", artist,title,value);
+        });
+    }
+    
     $("#login").show();
     $("#mainscreen").hide();
     
     $("#login_button").click(function() {
-        lastfm.getUserImage($("#username").val(), function(image) {
-            lastfm.getTopTracks($("#username").val(), function(topTracks) {
-                $("#login").hide();
-                $("#mainscreen").show();
-                for (var i=0;i<3;i++) {
-                    var trk=topTracks[i];
-                    $("#tracks").append(templates.trackElement(trk.artist,trk.track,trk.image));
-                }
-            });
+        var username=$("#username").val();
+        lastfm.getUserImage(username, function(image) {
+            socket.emit('join', {"user": username, "image": image});
+            lastfm.getTopTracks($("#username").val(), startRound);
         });
         return false;
     });
+    
+    
 
 });
--- frontend/js/templates.js
+++ frontend/js/templates.js
@@ -1,10 +1,11 @@
 var templates=function() {
 
-    function trackElement(artist,title,image) {
+    function trackElement(artist,title,image,value) {
         var element = $("#templates .track").clone();
         element.find(".artist").text(artist);
         element.find(".title").text(title);
         element.find("img").attr("src",image);
+        element.data("score",value);
         return element;
     }
     
